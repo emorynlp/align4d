7ad49a95e0b85965742cbf0688d9b45b92ad953d
qualidea1217 <peilinwu1217@gmail.com>
2023-05-24 20:08:24 -0400

    Update: ver. 1.2.2, no change in functionality, some changes in code comments

--- align4d/align.py
+++ align4d/align.py
@@ -105,47 +105,34 @@ def align_indices(output: dict) -> dict:
 
 
 def compile():
-    # Get the current script's directory
+    # Current script's directory, cpp directory, and build directory
     script_dir = os.path.dirname(os.path.realpath(__file__))
-
-    # Path to 'cpp' directory
     cpp_dir = os.path.join(script_dir, 'cpp')
-
-    # Path to 'build' directory
     build_dir = os.path.join(cpp_dir, 'build')
 
-    # Check if 'build' directory exists, remove it if it does
-    if os.path.exists(build_dir):
+    if os.path.exists(build_dir):  # pre-check if 'build' directory exists and remove it
         shutil.rmtree(build_dir)
 
-    # Check for files with extension '.so', '.pyd', or '.dll' in 'cpp' directory and remove them
+    # pre-check for files with extension '.so', '.pyd', or '.dll' in 'cpp' directory and remove them
     for ext in ['*.so', '*.pyd', '*.dll']:
         for f in glob.glob(os.path.join(script_dir, ext)):
-            os.chmod(f, 0o777)
+            os.chmod(f, 0o777)  # change the mod of the file to prevent access or permission error (not 100% work)
             os.remove(f)
 
-    # Change to 'cpp' directory
     os.chdir(cpp_dir)
-
-    # Run 'setup.py build' command
-    subprocess.check_call([sys.executable, 'setup.py', 'build'])
+    subprocess.check_call([sys.executable, 'setup.py', 'build'])  # Run 'setup.py build' command
 
     # Find directory that contains 'lib' in its name
     lib_dir = next((os.path.join(build_dir, d) for d in os.listdir(build_dir) if 'lib' in d), None)
     if lib_dir is None:
         raise FileNotFoundError('No directory containing "lib" was found in the build directory.')
 
-    # Find file with extension '.so', '.pyd', or '.dll'
-    file_path = next(
-        (os.path.join(lib_dir, f) for ext in ['*.so', '*.pyd', '*.dll'] for f in glob.glob(os.path.join(lib_dir, ext))),
-        None)
+    # Find file with extension '.so', '.pyd', or '.dll', there should be only one of them
+    file_path = next((os.path.join(lib_dir, f) for ext in ['*.so', '*.pyd', '*.dll'] for f in glob.glob(os.path.join(lib_dir, ext))), None)
     if file_path is None:
         raise FileNotFoundError('No file with extension .so, .pyd or .dll was found.')
 
-    # Copy the found file to the parent directory of the 'cpp' directory
-    shutil.copy(file_path, script_dir)
-
-    # Check if 'build' directory exists, remove it if it does
-    if os.path.exists(build_dir):
+    shutil.copy(file_path, script_dir)  # Copy the found file to the parent directory of the 'cpp' directory
+    if os.path.exists(build_dir):  # post-check if 'build' directory exists, remove it if it does
         shutil.rmtree(build_dir)
 
